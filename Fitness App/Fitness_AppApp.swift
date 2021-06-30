e7721837173bdd0a4228bdc966e7bb6c3093f8f9
Yaroslav Golinskiy <kaiserdem@gmail.com>
2021-06-30 12:01:19 +0300

    Firebase Setup

--- Fitness App/Fitness_AppApp.swift
+++ Fitness App/Fitness_AppApp.swift
@@ -6,12 +6,27 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct Fitness_AppApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
+    
+    
     var body: some Scene {
         WindowGroup {
             LandingView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        print("Setting up firebase")
+        FirebaseApp.configure()
+        return true
     }
 }
